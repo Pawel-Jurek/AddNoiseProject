b91ecd223c0b6b0a029cdf7f54a6197f9db98b90
Paweł Jurek <pj302000@student.polsl.pl>
2023-11-09 19:44:03 +0100

    Selecting image

--- JaProj/AddNoise/ViewModels/MainWindowViewModel.cs
+++ JaProj/AddNoise/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
 
 namespace AddNoise.ViewModels
 {
     public partial class MainWindowViewModel: ViewModelBase
     {
+
+        public MainWindowViewModel() 
+        { 
+        }
+
+
+
+        public RelayCommand SelectImage { get; private set; }
+        public void InitializeCommands()
+        {
+            InitializeSelectImage();
+        }
+
+
+        private void InitializeSelectImage()
+        {
+            SelectImage = new RelayCommand(() =>
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.InitialDirectory = "c:\\";
+                dlg.Filter = "Image files (*.bmp)|*.bmp|All Files (*.*)|*.*";
+                dlg.RestoreDirectory = true;
+                bool? wasOKButtonClicked = dlg.ShowDialog();
+
+                if (wasOKButtonClicked == null ? false : (bool)wasOKButtonClicked)
+                {
+                    string selectedFileName = dlg.FileName;
+
+                    Debug.WriteLine("Button clicked. Selected file: " + selectedFileName);
+                }
+            });
+        }
+
+
     }
 }
